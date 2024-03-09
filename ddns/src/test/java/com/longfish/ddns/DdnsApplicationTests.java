f10d647cc31f5743aa4fef2fae06804aa587e136
longfish <longfishes@qq.com>
2024-03-09 11:07:38 +0800

    ddns project stable-version-1.0.1

---
+++ ddns/src/test/java/com/longfish/ddns/DdnsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.longfish.ddns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello");
+    }
+
+}
