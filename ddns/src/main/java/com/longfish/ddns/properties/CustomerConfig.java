a64bf5bc4a754e674ad6d56e0df70b5d30633561
longfish <longfishes@qq.com>
2024-03-09 11:50:23 +0800

    update a new version of ddns

--- ddns/src/main/java/com/longfish/ddns/properties/CustomerConfig.java
+++ ddns/src/main/java/com/longfish/ddns/properties/CustomerConfig.java
@@ -10,4 +10,6 @@ import org.springframework.stereotype.Component;
 public class CustomerConfig {
 
     private String subDomain;
+
+    private String recordId;
 }
--- ddns/src/main/java/com/longfish/ddns/service/impl/DdnsServiceImpl.java
+++ ddns/src/main/java/com/longfish/ddns/service/impl/DdnsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.longfish.ddns.service.impl;
 
+import com.longfish.ddns.entity.RecordList;
 import com.longfish.ddns.entity.RecordListResp;
 import com.longfish.ddns.properties.AccessKey;
 import com.longfish.ddns.properties.CustomerConfig;
@@ -10,6 +11,9 @@ import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Service
 public class DdnsServiceImpl implements DdnsService {
@@ -19,25 +23,30 @@ public class DdnsServiceImpl implements DdnsService {
     @Autowired
     private CustomerConfig customerConfig;
 
-    private Long recordId;
+    private String recordValue;
 
     @Override
     public void update(String ip) {
         RecordListResp recordListResp = RequestUtil.executeQuery(accessKey.getSecretId(), accessKey.getSecretKey());
         Response resp;
+        List<RecordList> recordLists = new ArrayList<>();
 
         recordListResp.getRecordList().forEach(record -> {
             if (record.getName().equals(customerConfig.getSubDomain()) && record.getStatus().equals("ENABLE")) {
-                if (!record.getValue().equals(ip)) {
-                    recordId = record.getRecordId();
-                }
-                else {
-                    log.info("当前ip与解析记录相同！无需添加dns记录！");
-                    throw new RuntimeException("当前ip与解析记录相同！无需添加dns记录！");
-                }
+                if (!String.valueOf(record.getRecordId()).equals(customerConfig.getRecordId()))
+                    recordLists.add(record);
+                else recordValue = record.getValue();
             }
         });
-        resp = RequestUtil.executeDNS(accessKey.getSecretId(), accessKey.getSecretKey(), String.valueOf(recordId), ip, customerConfig.getSubDomain());
+        if (recordLists.size() != 0) {
+            log.error("子域名不可用！");
+            System.exit(-1);
+        } else {
+            if (recordValue.equals(ip)) {
+                throw new RuntimeException("当前解析与ip地址一致，无需添加dns记录！");
+            }
+        }
+        resp = RequestUtil.executeDNS(accessKey.getSecretId(), accessKey.getSecretKey(), customerConfig.getRecordId(), ip, customerConfig.getSubDomain());
         assert resp != null;
         if (resp.code() == 200) {
             log.info("{}", resp);
