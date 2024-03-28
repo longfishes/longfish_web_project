56d290586b6afdd78cdff3fd0aa495ad92fd8c7b
longfish <longfishes@qq.com>
2024-03-28 19:42:36 +0800

    rabbitmq-demo

---
+++ rabbitmq-demo/src/test/java/com/longfish/rabbitmqdemo/RabbitmqDemoApplicationTests.java
@@ -0,0 +1,33 @@
+package com.longfish.rabbitmqdemo;
+
+import com.longfish.rabbitmqdemo.pojo.EmailDTO;
+import com.longfish.rabbitmqdemo.util.EmailUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
+class RabbitmqDemoApplicationTests {
+
+    @Autowired
+    private EmailUtil emailUtil;
+
+    @Test
+    public void testSend() {
+        String username = "longfishes@qq.com";
+        Map<String, Object> map = new HashMap<>();
+        map.put("content", "测试2");
+        EmailDTO emailDTO = EmailDTO.builder()
+                .email(username)
+                .subject("测试标题")
+                .template("commontest.html")
+                .commentMap(map)
+                .build();
+        System.out.println(emailDTO);
+        emailUtil.sendHtmlMail(emailDTO);
+    }
+
+}
